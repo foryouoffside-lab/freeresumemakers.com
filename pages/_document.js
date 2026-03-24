d6332eea6c0feaf51ddbeb82436043c571a287b1
foryouoffside-lab <foryouoffside@gmail.com>
2026-03-24 11:58:36 +0530

    Remove Clarity script and manifest.json reference

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
