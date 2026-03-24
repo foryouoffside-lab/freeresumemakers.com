a9dfd4968fc236e8a02e34608093b65c739c5a9e
foryouoffside-lab <foryouoffside@gmail.com>
2026-03-24 11:30:17 +0530

    Add manifest.json, icon.svg, and fix mobile-web-app-capable

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+﻿import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
